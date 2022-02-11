cc56d34439b0c96f3abec095deddca93906ff514
Devin Stein <devstein@seas.upenn.edu>
2022-02-11 11:49:30 -0500

    Add ether and ether store

--- app/src/global.d.ts
+++ app/src/global.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="@sveltejs/kit" />
+
+// https://gitlab.com/clb1/svelte-ethers-store/-/issues/1
+declare module 'svelte-ethers-store' {
+	import type { Readable } from 'svelte/store';
+	import type { Provider } from '@ethersproject/abstract-provider';
+	import type { Signer } from '@ethersproject/abstract-signer';
+
+	interface DefaultEVMStore {
+		disconnect: () => void;
+		setProvider: () => void;
+		provider: Readable<Provider>;
+		signer: Readable<Signer>;
+		connected: Readable<boolean>;
+		chainId: Readable<number | string>;
+		signerAddress: Readable<string>;
+		evmProviderType: Readable<string>;
+	}
+
+	export const defaultEvmStores: DefaultEVMStore;
+	export const connected: DefaultEVMStore['connected'];
+	export const chainId: DefaultEVMStore['chainId'];
+	export const signerAddress: DefaultEVMStore['signerAddress'];
+	export const provider: DefaultEVMStore['provider'];
+	export const signer: DefaultEVMStore['signer'];
+}
