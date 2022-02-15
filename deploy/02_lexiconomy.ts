f68b9c848153a6b4f988ad9720dbd6d2434fb6d7
Devin Stein <devstein@seas.upenn.edu>
2022-02-14 20:02:33 -0800

    Deploy to Rinkeby Testnet  (#5)
    * Add OpenSea Proxy Registry to deploy script
    
    * Deploy to Rinkeby testnet
    
    * Ignore TS error
    
    * Add rinkeby contract addr

--- deploy/02_lexiconomy.ts
+++ deploy/02_lexiconomy.ts
@@ -1,4 +1,4 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types/runtime";
 import { DeployFunction } from "hardhat-deploy/types";
 
 import {
@@ -10,21 +10,45 @@ const CONTRACT_NAME = "Lexiconomy";
 
 export const tags = ["Lexiconomy"];
 
+const OpenSeaProxyRegistries = {
+  // Mainet
+  1: "0xa5409ec958c83c3f309868babaca7c86dcb077c1",
+  // Rinkeby
+  4: "0xf57b2c51ded3a29e6891aba85459d600256cf317",
+  // Polygon Mainet
+  137: "0x58807baD0B376efc12F5AD86aAc70E78ed67deaE",
+};
+
 const func: DeployFunction = async function ({
   getNamedAccounts,
   deployments,
+  getChainId,
 }: HardhatRuntimeEnvironment) {
-  const { deploy, get } = deployments;
+  const { deploy, get, execute } = deployments;
   const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
 
   const pricer = await get("FixedPricer");
   const validator = await get("LemmaValidator");
 
-  const lexiconomy = await deploy(CONTRACT_NAME, {
+  await deploy(CONTRACT_NAME, {
     from: deployer,
     args: [pricer.address, validator.address],
     log: true,
   });
+
+  // @ts-ignore
+  const proxyRegistryAddress = OpenSeaProxyRegistries[chainId];
+
+  // do nothing if there is no proxy
+  if (!proxyRegistryAddress) return;
+
+  await execute(
+    CONTRACT_NAME,
+    { from: deployer },
+    "setProxyRegistryAddress",
+    proxyRegistryAddress
+  );
 };
 
 export default func;
