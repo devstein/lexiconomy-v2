81b0a789d11de4a3213ecdfe1926e88157c15404
Devin Stein <devstein@seas.upenn.edu>
2022-02-22 18:46:44 -0800

    Get route for explore

--- app/src/routes/explore/index.svelte
+++ app/src/routes/explore/index.svelte
@@ -1,47 +1,5 @@
 <script lang="ts" context="module">
 	export const prerender = true;
-
-	import { getContract } from '$lib/web3/contract';
-
-	const items = [
-		'lemma',
-		'lexiconomy',
-		'characters',
-		'nnmbl',
-		'something',
-		'another',
-		'g',
-		'h',
-		'something really long',
-		'a really really really really long'
-	];
-
-	// TODO: MOVE TO /[explore].ts file!
-	export async function load({ params }) {
-		const { lemma } = params;
-
-		const contract = await getContract();
-
-		const filter = contract.filters.Invent();
-		const events = await contract.queryFilter(filter);
-		const lemmas = events.map(({ args: { lemma } }) => lemma);
-
-		// should we use a global store to share between pages?
-		// how do we mutate once lemma is invented or a new definition/example is created
-		return {
-			props: {
-				lemmas
-			}
-		};
-	}
-
-	// on load
-	// list all tokens for the Lexiconomy
-	// naive: invent events
-	// OpenSea API?
-	// Get example and definition for all lemmas
-
-	// generate color based off word
 </script>
 
 <script lang="ts">
@@ -51,7 +9,7 @@
 <h1 class="text-5xl pb-4 mb-8 border-b-4 border-green-400">explore</h1>
 <div class="px-8 py-4 h-full">
 	<ul class="h-full grid grid-cols-5 gap-5 place-items-center break-words">
-		{#each items as item}
+		{#each lemmas as item}
 			<li class="h-4/5 w-4/5 scene">
 				<div class="card p-2 rounded">
 					<a
@@ -73,30 +31,8 @@
 			</li>
 		{/each}
 	</ul>
-	<ul class="h-full grid grid-cols-5 gap-5 place-items-center break-words">
-		{#each lemmas as item}
-			<li class="h-4/5 w-4/5 scene">
-				<div class="card p-2 rounded">
-					<a
-						href="/lemma/{item}"
-						class="text-center text-lg font-semibold card__face font-semibold flex flex-col justify-center items-center"
-					>
-						<div class="mx-2 font-mono">
-							{item}
-						</div>
-					</a>
-					<a href="/lemma/{item}" class="card__face card__face--back">
-						<div class="m-4 space-y-2">
-							<div>the world's decentralized dictionary</div>
-							<div class="italic">i coined that phrase on the lexiconomy</div>
-						</div>
-					</a>
-				</div>
-			</li>
-		{/each}
-	</ul>
-	<div>Navigate pages</div>
 </div>
+<div class="my-8">Navigate pages</div>
 
 <style>
 	/* Add an arrow for definition  */
@@ -110,6 +46,8 @@
 
 	.scene {
 		perspective: 1000px;
+		width: 350px;
+		height: 350px;
 	}
 
 	.card {
---
+++ app/src/routes/explore/index.ts
@@ -0,0 +1,20 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+import { getContract } from '$lib/web3/contract';
+
+export const get: RequestHandler = async () => {
+	const contract = await getContract();
+
+	console.time(`get: explore - invent events`);
+	const filter = contract.filters.Invent();
+	const events = await contract.queryFilter(filter);
+	console.timeEnd(`get: explore - invent events`);
+
+	const lemmas = events.map(({ args: { lemma } }) => lemma);
+
+	return {
+		body: {
+			lemmas
+		}
+	};
+};
