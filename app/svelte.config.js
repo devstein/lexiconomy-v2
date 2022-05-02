a87c1ac98c2087f0eca8ff017bfcd34b621c6465
Devin Stein <devstein@seas.upenn.edu>
2022-05-01 17:07:00 -0700

    Allow CORs

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,6 +1,30 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+/** @type {import('vite').Plugin} */
+const cors = {
+	name: 'cors',
+	configureServer(server) {
+		// TODO: Use only one
+		server.headers = {
+			'Access-Control-Allow-Credentials': 'true',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET,OPTIONS',
+			'Access-Control-Allow-Headers':
+				'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization'
+		};
+		server.middlewares.use((req, res, next) => {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+			res.setHeader(
+				'Access-Control-Allow-Headers',
+				'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization'
+			);
+			return next();
+		});
+	}
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,7 +32,10 @@ const config = {
 	preprocess: [preprocess({})],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			plugins: [cors]
+		}
 	}
 };
 
