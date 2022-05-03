f2dad627be5324d45b7a35fe5dc4ad4d977419b3
Devin Stein <devstein@seas.upenn.edu>
2022-05-02 19:09:03 -0700

    Add generation to metadata

--- app/src/routes/token/[id]/index.ts
+++ app/src/routes/token/[id]/index.ts
@@ -4,6 +4,7 @@ import { getContract } from '$lib/web3/contract';
 
 // TODO: Update before going to production
 const BASE_URL = 'https://lexiconomy-v2.vercel.app';
+const NUMBER_OF_V1_TOKENS = 160;
 
 export const get: RequestHandler = async ({ params }) => {
 	const contract = await getContract();
@@ -24,19 +25,20 @@ export const get: RequestHandler = async ({ params }) => {
 		};
 	}
 
-	const description = `definition
-	${definition}
+	const description = `
+	# ${lemma}
+	## definition
+${definition}
 
-	example
-	${example}`;
+## example
+${example}`;
 
 	// - [ ] animation_url -> TBD!
 	// - [ ] background color?
 	// - [ ] Attributes
 	// - [ ] inverted? rare
-	// - [ ] generation?
 	// - [ ] back_color
-	// - [ ] number
+	// - [ ] color
 
 	// compute rarity based off some hash??
 	// change colors accordingly (gradient, kaleidescope, etc)
@@ -54,6 +56,11 @@ export const get: RequestHandler = async ({ params }) => {
 					display_type: 'number',
 					trait_type: 'Number',
 					value: number.toString()
+				},
+				{
+					trait_type: 'Generation',
+					// number < NUMBER_OF_V1_TOKENS then it's a 1  else 2
+					value: number.isLessThan(NUMBER_OF_V1_TOKENS) ? 'v1' : 'v2'
 				}
 			]
 		}
