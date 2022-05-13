bd76167ab1fd2f3ee06d3aa6346ab07d95bce881
Devin Stein <devstein@seas.upenn.edu>
2022-05-13 10:55:26 -0700

    Mobile menu design (non functioning)

--- app/src/lib/components/Header.svelte
+++ app/src/lib/components/Header.svelte
@@ -9,30 +9,62 @@
 	};
 </script>
 
-<header class="flex flex-row items-center font-mono space-x-4">
-	<a href="/home" class="text-2xl">lexiconomy</a>
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-		class="nav-search-icon"
-	>
-		<circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" />
-	</svg>
-	<form on:submit|preventDefault={submit} class="flex-1">
-		<input
-			bind:value={search}
-			label="Label"
-			class="w-full focus:bg-slate-100 focus-visible:outline-none"
-		/>
-	</form>
-	<a class="text-xl" href="/explore">explore</a>
-	<a class="text-xl" href="/guide">guide</a>
-	<a class="text-xl" href="/manifesto">manifesto</a>
+<header class="my-4 mx-2">
+	<div class="hidden md:flex flex-row items-center font-mono space-x-4">
+		<a href="/home" class="text-2xl">lexiconomy</a>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="nav-search-icon"
+		>
+			<circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" />
+		</svg>
+		<form on:submit|preventDefault={submit} class="flex-1">
+			<input
+				bind:value={search}
+				label="Label"
+				class="w-full focus:bg-slate-100 focus-visible:outline-none"
+			/>
+		</form>
+		<a class="text-xl" href="/explore">explore</a>
+		<a class="text-xl" href="/guide">guide</a>
+		<a class="text-xl" href="/manifesto">manifesto</a>
+	</div>
+
+	<!-- Hamburger menu -->
+	<div class="flex flex-row md:hidden space-x-4">
+		<div class="space-y-2">
+			<span class="block w-8 h-0.5 bg-gray-600" />
+			<span class="block w-8 h-0.5 bg-gray-600" />
+			<span class="block w-8 h-0.5 bg-gray-600" />
+		</div>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="nav-search-icon"
+		>
+			<circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" />
+		</svg>
+		<form on:submit|preventDefault={submit} class="flex-1">
+			<input
+				bind:value={search}
+				label="Label"
+				class="w-full focus:bg-slate-100 focus-visible:outline-none"
+			/>
+		</form>
+	</div>
 </header>
