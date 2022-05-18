f5968700ab00684bd7038e849898f491a01c7ccb
Devin Stein <devstein@seas.upenn.edu>
2022-05-18 17:48:30 -0400

    Update home design

--- app/src/routes/home/index.svelte
+++ app/src/routes/home/index.svelte
@@ -5,13 +5,13 @@
 </script>
 
 <div class="h-full flex flex-col items-center justify-center text-center">
-	<div class="space-y-1 md:space-y-3">
+	<div class="space-y-3 md:space-y-4">
 		<h1 class="font-mono text-5xl md:text-7xl">lexiconomy</h1>
 		<h3 class="font-light	text-lg md:text-xl">/ leksi-kän-əmē /</h3>
 		<h3 class="font-light	text-lg md:text-xl italic">noun</h3>
-		<h2 class="text-xl md:text-2xl">1. the world's decentralized dictionary.</h2>
+		<h2 class="font-light text-xl md:text-2xl">1. the world's decentralized dictionary.</h2>
 	</div>
-	<div class="mt-8 w-4/5 lg:w-3/5 space-y-8">
+	<div class="mt-12 w-4/5 lg:w-3/5 space-y-8">
 		<GlobalSearchBar />
 		<div class="font-mono grid grid-rows-4 text-lg md:grid-cols-4 md:text-xl">
 			<a class="p-4 md:p-8 hover:bg-slate-100 focus:bg-slate-100" href="/explore">explore</a>
