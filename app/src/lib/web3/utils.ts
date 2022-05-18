8726f79387eeeff76532609044c13b0ed0621768
Devin Stein <devstein@seas.upenn.edu>
2022-05-18 17:49:03 -0400

    isZeroAddr helper

--- app/src/lib/web3/utils.ts
+++ app/src/lib/web3/utils.ts
@@ -1,1 +1,4 @@
 export const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
+
+export const isZeroAddr = (address: string): boolean =>
+	address.toLowerCase() === ZERO_ADDR.toLowerCase();
