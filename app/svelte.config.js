bb14ade769b1cd88ca3a8a5e669ee5d82b6a9d40
Devin Stein <devstein@seas.upenn.edu>
2022-05-21 14:56:52 -0400

    Upgrade sveltekit and support web3modal via Vite

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+const production = process.env.NODE_ENV === 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.svx', '.md'],
@@ -10,7 +13,27 @@ const config = {
 	preprocess: [preprocess({}), mdsvex({ extensions: ['.svx', '.md'] })],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			plugins: [
+				// ↓ Needed for development mode
+				!production &&
+					nodePolyfills({
+						include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js')]
+					})
+			],
+			build: {
+				rollupOptions: {
+					plugins: [
+						// ↓ Needed for build
+						nodePolyfills()
+					]
+				},
+				commonjsOptions: {
+					transformMixedEsModules: true
+				}
+			}
+		}
 	}
 };
 
